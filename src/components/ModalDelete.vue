89c11c740379a41665ae9c0a7fa587586b49bd5c
Nader Farhat <n-farhat@hotmail.com>
2022-01-22 22:33:48 -0300

    init

---
+++ src/components/ModalDelete.vue
@@ -0,0 +1,190 @@
+<script>
+export default {
+  name: "ModalInsert",
+  props: ["listContact", "phoneId"],
+  data() {
+    return {
+      id: "",
+      name: "",
+      email: "",
+      phone: "",
+      last: false,
+    };
+  },
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+    sendData() {
+      let newList = this.listContact;
+      newList.splice(this.phoneId - 1, 1);
+      this.listContact = newList;
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<template>
+  <transition name="modal-fade">
+    <div class="modal-backdrop">
+      <div
+        class="modal_delete"
+        role="dialog"
+        aria-labelledby="modalTitle"
+        aria-describedby="modalDescription"
+      >
+        <header class="modal_header-delete" id="modalTitle">
+          <div name="header" class="header_delete">Excluir contato</div>
+        </header>
+
+        <section class="modal-body-delete" id="modalDescription">
+          <div name="body" class="body">
+            Deseja realmente excluir o contato?
+          </div>
+        </section>
+
+        <footer class="modal-footer">
+          <button
+            type="button"
+            class="btn_close-delete"
+            @click="close"
+            aria-label="Close modal"
+          >
+            Cancelar
+          </button>
+
+          <button
+            type="button"
+            class="btn-orange"
+            @click="sendData"
+            aria-label="Close modal"
+          >
+            Excluir
+          </button>
+        </footer>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<style>
+.header_delete {
+  width: 106px;
+  height: 19px;
+  margin: 0 260px 0px 16px;
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #2a2d3b;
+}
+
+.body {
+  color: #2a2d3b;
+  font-family: Roboto;
+  text-align: left;
+  font-size: 14px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+}
+
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal_delete {
+  background: #ffffff;
+  box-shadow: 2px 2px 20px 1px;
+  overflow-x: auto;
+  display: flex;
+  flex-direction: column;
+  width: 432px;
+  height: 207px;
+  border-radius: 16px;
+}
+
+.modal-header,
+.modal-footer {
+  display: flex;
+  flex-direction: row;
+}
+
+.modal_header-delete {
+  position: relative;
+  border-bottom: 1px solid #c0c3d2;
+  justify-content: space-between;
+  padding: 12.5px 0px !important;
+}
+
+.modal-footer {
+  border-top: 1px solid #c0c3d2;
+  flex-direction: column;
+}
+
+.modal-body-delete {
+  position: relative;
+  padding: 20px 24px 57px 24px;
+}
+
+.btn_close-delete {
+  position: relative !important;
+  width: 80px !important;
+  top: 0;
+  right: 0;
+  background-color: transparent;
+  font-family: Roboto;
+  font-size: 16px;
+  padding: 0px !important;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: center;
+  box-shadow: none;
+  margin: 0px 0px 0 60px !important;
+  color: #fa7268 !important;
+  cursor: pointer;
+}
+
+.btn-orange {
+  width: 80px;
+  height: 32px;
+  margin: 1px 1px 1px 0;
+  padding: 5px 5px;
+  border-radius: 16px;
+  box-shadow: inset 0 0 0 1px rgba(255, 255, 255, 0.16),
+    0 0 0 0.5px rgba(0, 0, 0, 0.08), inset 0 0 0 0.5px rgba(0, 0, 0, 0.08),
+    0 2px 4px 0.5px rgba(0, 0, 0, 0.16);
+  background-color: #fa7268;
+  font-size: 14px;
+  font-weight: 500;
+  cursor: pointer;
+  color: #ffffff;
+}
+
+.modal-fade-enter,
+.modal-fade-leave-to {
+  opacity: 0;
+}
+
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+</style>
---
+++ src/components/PhoneList.vue
@@ -0,0 +1,254 @@
+<template>
+  <div id="phone-list">
+    <div class="header_table">
+      <div class="title_header contact_title">Contatos</div>
+      <div class="title_header item_header">E-mail</div>
+      <div class="title_header item_header-phone">Telefone</div>
+    </div>
+    <span class="container_list">
+      <div
+        id="phone"
+        v-for="phone in listContact"
+        v-bind:key="phone.id"
+        :class="[phone.last === true ? 'new_item' : 'item_list']"
+      >
+        <div id="circle" class="col_circle circle" :style="cssVars">
+          {{ getLetter(phone) }}
+        </div>
+        <div id="name" class="name item_name">{{ phone.name }}</div>
+        <div id="email" class="name item_header">{{ phone.email }}</div>
+        <div id="phone" class="name item_header-phone-list">
+          {{ phone.phone }}
+        </div>
+        <span class="container_buttons">
+          <div class="col_none">
+            <img
+              src="../assets/ic-edit.png"
+              class="edit-button"
+              alt="pic"
+              id="img"
+              v-on:click="showModalEditFunc"
+            />
+          </div>
+          <div class="col_none">
+            <img
+              src="../assets/ic-delete.png"
+              class="edit-button"
+              alt="pic"
+              id="img"
+              v-on:click="showModalDeleteFunc"
+            />
+          </div>
+        </span>
+        <ModalDelete
+          v-show="showModalDelete"
+          @close="closeModalDeleteFunc"
+          v-bind:key="key"
+          :listContact="listContact"
+          :phoneId="phone.id"
+        >
+        </ModalDelete>
+        <ModalEdit
+          v-show="showModalEdit"
+          @close="closeModalEditFunc"
+          v-bind:key="key"
+          :listContact="listContact"
+          :phoneId="phone.id"
+        >
+        </ModalEdit>
+      </div>
+    </span>
+  </div>
+</template>
+
+<script>
+import ModalDelete from "./ModalDelete";
+import ModalEdit from "./ModalEdit.vue";
+
+export default {
+  name: "list",
+  props: ["listContact"],
+  data() {
+    return {
+      showModalDelete: false,
+      showModalEdit: false,
+    };
+  },
+  computed: {
+    cssVars() {
+      return {
+        "background-color": this.getColor(),
+      };
+    },
+  },
+  methods: {
+    delete() {
+      this.$emit("close");
+    },
+    edit() {
+      let teste = this.listContact.push({
+        id: this.listContact.length + 1,
+        name: "",
+        email: "",
+        phone: 1,
+      });
+      this.listContact = teste;
+      this.$emit("close");
+    },
+    showModalDeleteFunc() {
+      this.showModalDelete = true;
+    },
+    closeModalDeleteFunc() {
+      this.showModalDelete = false;
+    },
+    showModalEditFunc() {
+      this.showModalEdit = true;
+    },
+    closeModalEditFunc() {
+      this.showModalEdit = false;
+    },
+    getColor() {
+      let n = (Math.random() * 0xfffff * 1000000).toString(16);
+      return "#" + n.slice(0, 6);
+    },
+    getLetter(phone) {
+      return phone.name.charAt(0);
+    },
+  },
+  components: {
+    ModalDelete,
+    ModalEdit,
+  },
+};
+</script>
+
+<style scoped>
+.header_table {
+  width: 100%;
+  height: 40px;
+  margin: 16px 0px 1px 0px;
+  padding: 16px 0px 9px 8px;
+  border-radius: 4px;
+  border: solid 1px #e1e1e1;
+  background-color: #fff;
+  display: flex;
+  justify-content: left;
+}
+.title_header {
+  color: #9198af;
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  min-width: 300px;
+}
+
+.title_header-none-image {
+  color: #9198af;
+  width: 24px;
+  margin-right: 16px;
+}
+
+.item_name {
+  width: 300px;
+}
+
+.title_header-none {
+  flex-basis: 20%;
+}
+
+.contact_title {
+  float: left;
+  margin-left: 47px;
+}
+
+.item_header {
+  margin-left: 175px;
+}
+
+.item_header-phone {
+  margin-left: 215px;
+}
+
+.item_header-phone-list {
+  margin-left: 355px;
+}
+
+.edit-button {
+  cursor: pointer;
+  padding: 0px 16px;
+}
+
+.item_phone {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.item_list {
+  width: 100%;
+  margin: 0px 0px 1px 0px;
+  padding: 8px 0px 8px 8px;
+  border-radius: 4px;
+  border: solid 1px #e1e1e1;
+  background-color: #ffffff;
+  display: flex;
+}
+
+.container_list {
+  max-height: 40px;
+}
+
+.new_item {
+  width: 100%;
+  margin: 0px 0px 1px 0px;
+  padding: 8px 0px 8px 8px;
+  border-radius: 4px;
+  border: solid 1px #e1e1e1;
+  background-color: #ffffff;
+  display: flex;
+  background: #ffffff;
+  animation: mymove 10s;
+}
+
+@keyframes mymove {
+  from {
+    background-color: #fff3f2;
+  }
+  to {
+    background-color: #ffffff;
+  }
+}
+
+.item_list:hover {
+  background-color: #fff3f2;
+}
+
+.circle {
+  height: 24px;
+  width: 24px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 16px;
+  font-weight: 500;
+  color: #fff;
+}
+
+.container_buttons {
+  right: 16px;
+  display: flex;
+  position: absolute;
+}
+
+.col_circle {
+  width: 25px;
+  margin-right: 16px;
+}
+
+.name {
+  display: flex;
+  justify-content: left;
+  align-self: center;
+}
+</style>
